94f7f3abc61ee1125c79e035fd3e2f17355fd0f7
Daniel Palacio <danpal@gmail.com>
2012-11-10 21:56:03 -0500

    Make the blog be configurable by YAML

--- app/blog.rb
+++ app/blog.rb
@@ -1,17 +1,24 @@
 require 'bundler'
 Bundler.require
 require 'sinatra/partial'
+require './app/config'
 
 module Galleta
   class Blog < Sinatra::Base
     register Sinatra::Partial    
 
   set :partial_template_engine, :haml    
+  
+  @@config = Galleta::Config.new #loads ./config/config.yaml
+
 
   ##
   # Blog index. Lists all posts
   ##
   get '/' do
+    @description = @@config.description
+    @keywords = @@config.keywords
+    @title  = @@config.title
     haml :index
   end
 
---
+++ app/config.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+module Galleta
+  class Config
+    attr_accessor :title, :description, :keywords  
+    def initialize
+      config = YAML.load_file('./config/config.yml')
+      @title = config["blog"]["title"]
+      @description = config["blog"]["description"]
+      @keywords = config["blog"]["keywords"]
+    end
+  end
+end
